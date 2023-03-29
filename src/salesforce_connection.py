9af898bd49ce9eed7cbf3ef1f0d674c3a5be5825
Leo Jokinen <leo.jokinen81@gmail.com>
2023-03-29 16:00:05 +0300

    Added connection

---
+++ src/salesforce_connection.py
@@ -0,0 +1,44 @@
+import os
+from simple_salesforce import Salesforce
+
+DEFAULT_API_VERSION = '57.0'
+
+class SalesforceConnection:
+    """Create new connection to Salesforce"""
+
+    def __init__(
+        self,
+        username=None,
+        password=None,
+        security_token=None,
+        domain=None,
+        version=DEFAULT_API_VERSION):
+        self.conn = None
+
+        # Use environment variables in case if username was not given
+        if username is None:
+            self.username = os.environ["SF_USER"]
+            self.password = os.environ["SF_PASS"]
+            self.security_token = os.environ["SF_SECURITY_TOKEN"]
+            self.domain = os.environ["SF_DOMAIN"]
+        else:
+            self.username = username
+            self.password = password
+            self.security_token = security_token
+            self.domain = domain
+        
+        self.version = version
+
+    def get_conn(self):
+        """
+        Sign into Salesforce, only if we are not already signed in.
+        """
+        if not self.conn:
+            self.conn = Salesforce(
+                username=self.username, 
+                password=self.password, 
+                security_token=self.security_token, 
+                domain=self.domain,
+                version=self.version,
+            )
+        return self.conn
